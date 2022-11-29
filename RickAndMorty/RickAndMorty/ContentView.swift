79960860662feda42506be4313bc552839b19b22
Stefan Bayne <stefanbayne@gmail.com>
2022-11-28 19:03:41 -0500

    Second commit.

---
+++ RickAndMorty/RickAndMorty/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Stefan Bayne on 11/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Stefan Bayne on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
